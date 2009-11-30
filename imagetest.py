268374b27026788a3c2bfd327113120383e0e2c8
Robert (Jamie) Munro <rjmunro@arjam.net>
2009-11-30 18:10:21 +0000

    Pull in images from list of plays, making unique and sorting by (title, url)

--- imagetest.py
+++ imagetest.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # vim: set fileencoding=UTF-8 :
 from imageList import imageList,getTitleOfImage
+from operator import itemgetter
+from plays import plays
 print """<html>
 <head>
   <title>Image title test</title>
@@ -11,7 +13,11 @@ print """<html>
 <body>
 """
 
-for url,title in imageList.items():
+uniquePlays = list(set([(play['title'],play['titleImg']) for play in plays if play['titleImg']]))
+uniquePlays.sort()
+
+#for url,title in imageList.items():
+for title,url in uniquePlays:
   print '<div><img src="http://www.its-behind-you.com/%s" /><br />%s<br />%s</div>' % (url,url,getTitleOfImage(url))
 
 print """</body>
